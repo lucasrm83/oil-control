6e9f695e327931b96b1fb42c61896ee47765f4f5
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-06-18 19:57:02 -0300

    Oil types added

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        //production cost deve ser por ml, 50ml ou outra medida?
         System.out.println("Digite o preço do kg da semente: ");
         double precoKg = sc.nextDouble();
         System.out.println("Digite a quantidade em mls de óleo ao prensar 1kg da semente: ");
         double quantidadeMl = sc.nextDouble();
-        Oil oil = new Oil()
+        //Oil oil = new Oil(precoKg,);
     }
 }
